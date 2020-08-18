af29fc320fbffc5f5bb3491ee721d188954697a6
pkarthikldu <69841869+pkarthikldu@users.noreply.github.com>
2020-08-18 18:12:05 +1000

    Add files via upload

--- TestDB.php
+++ TestDB.php
@@ -6,9 +6,12 @@ $password = "admin";
 // Create connection
 $conn = new mysqli($servername, $username, $password);
 
+
+echo "Before Connection";
+
 // Check connection
 if ($conn->connect_error) {
-  die("Connection failed: " . $conn->connect_error);
+  echo("Connection failed: " . $conn->connect_error);
 }
 echo "Connected successfully";
 ?>
