a4724fea3f054e0d9b74fdb357caf4b1776be1cd
pkarthikldu <69841869+pkarthikldu@users.noreply.github.com>
2020-08-18 18:39:01 +1000

    Update TestDB.php

--- TestDB.php
+++ TestDB.php
@@ -1,10 +1,17 @@
 <?php
 $servername = getenv("MYSQL_SERVICE_HOST");
-$username = "admin";
-$password = "admin";
+$port= getenv("MYSQL_SERVICE_PORT");
+$username = getenv("DBusername");
+$password = getenv("DBPassword");
+$db=getenv("DBName");
 
+echo "$servername::"+.$servername;
+echo "$username::"+.$username;
+echo "$password::"+.$password;
+echo "$db:::"+.$db;
+echo "Before Connection";
 // Create connection
-$conn = new mysqli($servername, $username, $password);
+$conn = new mysqli($servername,$username,$password, $db);
 
 
 echo "Before Connection";
